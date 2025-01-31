3d3d463f95517fe054f0c6744799f9e3b5b8d264
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-01-30 19:30:38 -0600

    Complete part 3 of comparisons.rb

--- comparisons.rb
+++ comparisons.rb
@@ -60,33 +60,33 @@ puts "is number_students less than or equal to 21?", number_students <= 21
 
 puts 4 < 9
 #YOU DO: Explain.
-This line of code prints the result of the comparison: is the integer 4 less than the integer 9?
-The result is true. 
+#This line of code prints the result of the comparison: is the integer 4 less than the integer 9?
+#The result is true. 
 
 books = 3
 puts 4 < books
 # YOU DO: Explain.
-This line of code defines the variable books as 3. The next line of code prints the result of 
-the comparison: is the integer 4 less than the variable books?
-The result is false. 
+#This line of code defines the variable books as 3. The next line of code prints the result of 
+#the comparison: is the integer 4 less than the variable books?
+#The result is false. 
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
-The first line of code define the variable friends as 6. 
-The second line of code define the variable siblings as 2. 
-The third line of code puts the comparison: is friends greater than siblings?
-The result is true. 
+#The first line of code define the variable friends as 6. 
+#The second line of code define the variable siblings as 2. 
+#The third line of code puts the comparison: is friends greater than siblings?
+#The result is true. 
 
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
-The first line of code defines the variable attendees as 9. 
-The second line of code defines the variable meals as 8. 
-The third line of code puts the comparison: is attendees not equal to meals?
-The result is true.
+#The first line of code defines the variable attendees as 9. 
+#The second line of code defines the variable meals as 8. 
+#The third line of code puts the comparison: is attendees not equal to meals?
+#The result is true.
 
 #-------------------
 # PART 3: Logical Operators
@@ -108,16 +108,25 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
+puts loves_to_play || loves_dog_park
 
 
 # Determine if the dog loves to play and is a puppy
 # Assume that a dog is a puppy if it is less than 2 years old.
+dog_is_a_puppy = true
+if age < 2 
+else false 
+end
+
+puts loves_to_play && dog_is_a_puppy
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: It resulted in true. After I created the variable dog_is_a_puppy = true, 
+#I incorported an if and else statement to create the necessary parameters to the code to 
+#to allow me to properly run the comparison.
